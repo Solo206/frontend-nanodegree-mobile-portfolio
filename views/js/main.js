4024948a05a232275e0930fa34c3273dbb312520
Solo206 <jsolk206@gmail.com>
2015-01-22 22:17:38 -0800

    test2

--- views/js/main.js
+++ views/js/main.js
@@ -469,10 +469,11 @@ var resizePizzas = function(size) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
+window.requestAnimationFrame(function(){
 for (var i = 2; i < 100; i++) {
   var pizzasDiv = document.getElementById("randomPizzas");
   pizzasDiv.appendChild(pizzaElementGenerator(i));
-}
+}});
 
 // User Timing API again. These measurements tell you how long it took to generate the initial pizzas
 window.performance.mark("mark_end_generating");
@@ -525,9 +526,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  console.log("hi");
   for (var i = 0; i < 200; i++) {
-
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza-krak.png";
