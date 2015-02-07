fef2b4b116d7ddcb928d1a5039832f64975c7382
Solo206 <jsolk206@gmail.com>
2015-02-07 15:44:10 -0800

    update48

--- views/js/main.js
+++ views/js/main.js
@@ -514,11 +514,11 @@ function updatePositions() {
   // console.log(scrollOnTop);
   for (var i = 0; i < lengthOfItems; i++) {
     var phase = Math.sin((scrollOnTop) + (i % 5));
-    console.log(phase);
+    // console.log(phase);
     // items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
     // console.log(items[i].basicLeft);
     // console.log(items[i].basicLeft + (100 * phase + 'px'));
-    items[i].style.left=1000+'px';
+    items[i].style.left=1700+'px';
     // items[i].style.transform = 'translateX(' + (100 * scrollOnTop + (i % 5)) + 'px)';
   }
 
