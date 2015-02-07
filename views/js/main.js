791b6865ce5f6a8267496b87885e693c0bd122d8
Solo206 <jsolk206@gmail.com>
2015-02-06 22:55:39 -0800

    update 30

--- views/js/main.js
+++ views/js/main.js
@@ -537,7 +537,7 @@ window.addEventListener('scroll', updatePositions);
 
   var pizzaContainer=document.querySelector("#movingPizzas1");
   pizzaContainer.display='none';
-  for (var i = 0; i < 40; i++) {
+  for (var i = 0; i < 300; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza-krak.png";
