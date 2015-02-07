94e0506b688e969ec956379378f810ad0d90328f
Solo206 <jsolk206@gmail.com>
2015-02-06 19:54:35 -0800

    update 24

--- views/js/main.js
+++ views/js/main.js
@@ -514,7 +514,7 @@ function updatePositions() {
   for (var i = 0; i < lengthOfItems; i++) {
     // var phase = Math.sin((scrollOnTop) + (i % 5));
     // items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
-    items[i].style.transform = 'translateX(' + (100 * scrollOnTop + (i % 5)) + 'px)';
+    items[i].style.transform = 'translateX(' + (300 * scrollOnTop + (i % 5)) + 'px)';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
